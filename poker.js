cc196a739ca6380e53c8c2351b8d3a48b1f512ce
DivineUX23 <divineigbinoba23@gmail.com>
2025-01-05 22:14:02 +0100

    damn

---
+++ poker.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const prompt_sync_1 = __importDefault(require("prompt-sync"));
+const prompt = (0, prompt_sync_1.default)();
+console.log("Players fund is $100");
+const bet = prompt("Enter your bet ");
+console.log(`Players bet is $${bet}`);
---
+++ poker.ts
@@ -0,0 +1,17 @@
+import PromptSync from "prompt-sync";
+const prompt = PromptSync();
+
+console.log("Players fund is $100")
+
+const bet = prompt("Enter your bet ")
+
+console.log(`Players bet is $${bet}`)
+
+const total_cards = 52
+
+2-10
+
+const Jack = 10
+const Queen = 10
+const King = 10
+const Aces = 1 | 11
